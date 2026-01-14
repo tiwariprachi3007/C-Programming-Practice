8adb1cdbb5950226972888ea5aef3ed73c2a9ad6
Prachi Tiwari <tiwariprachi3007@gmail.com>
2026-01-14 22:12:27 +0530

    Initial commit with C programs

---
+++ 2d_array.c/rowSum_colSum.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main() {
+    int i, j, r, c, rsum, csum, arr[10][10];
+
+    printf("Enter the number of rows and columns: ");
+    scanf("%d%d", &r, &c);
+
+    printf("Enter the matrix elements:\n");
+    for(i = 0; i < r; i++) {
+        for(j = 0; j < c; j++) {
+            scanf("%d", &arr[i][j]);
+        }
+    }
+
+    // Row sums
+    for(i = 0; i < r; i++) {
+        rsum = 0;
+        for(j = 0; j < c; j++) {
+            rsum += arr[i][j];
+        }
+        printf("Sum of row %d: %d\n", i+1, rsum);
+    }
+
+    // Column sums
+    for(i = 0; i < c; i++) {
+        csum = 0;
+        for(j = 0; j < r; j++) {
+            csum += arr[j][i];
+        }
+        printf("Sum of column %d: %d\n", i+1, csum);
+    }
+
+    return 0;
+}
