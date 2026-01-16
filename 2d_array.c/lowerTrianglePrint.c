9aeff0555e6d2684849e715421b4fbe43f8229d2
Prachi Tiwari <tiwariprachi3007@gmail.com>
2026-01-16 22:19:28 +0530

    add

---
+++ 2d_array.c/lowerTrianglePrint.c
@@ -0,0 +1,28 @@
+#include <stdio.h> 
+int main() 
+{ 
+    int i, j, r, c, arr[10][10], rsum, csum; 
+    printf("Enter the Row and Column: "); 
+    scanf("%d%d",&r,&c); 
+    printf("Enter the Matrix: "); 
+    for(i=0; i<=r-1; i++) 
+    { 
+        for(j=0; j<=c-1; j++) 
+        { 
+            scanf("%d",&arr[i][j]); 
+        } 
+    } 
+    printf("Lower Triangle\n"); 
+    for(i=0; i<=r-1; i++) 
+    { 
+        for(j=0; j<=c-1; j++) 
+        { 
+            if(i>=j) 
+            { 
+                printf("%d ",arr[i][j]); 
+            } 
+        } 
+        printf("\n"); 
+    } 
+    return 0; 
+}
