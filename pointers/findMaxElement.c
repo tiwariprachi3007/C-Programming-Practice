c96d9521ff7d22f54a14886e8f20ab4f645d19ba
Prachi Tiwari <tiwariprachi3007@gmail.com>
2026-01-28 21:23:54 +0530

    pointers

---
+++ pointers/findMaxElement.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){
+    int n,i;
+    int arr[100];
+    int *ptr;
+    int max;
+
+    printf("enter the number of elements:");
+    scanf("%d",&n);
+    printf("enter the elements:\n");
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    ptr=arr;
+    max=*ptr; //let the first element is the max
+
+    //traverse the array using pointer
+    for(i=1;i<n;i++){
+        ptr++;
+        if(*ptr>max){
+            max=*ptr;
+
+        }
+    }
+    printf("maximum element is %d\n",max);
+    return 0;
+}
---
+++ pointers/swapCallReference.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+void swap(int *x,int *y){
+    int temp;
+    temp=*x;
+    *x=*y;
+    *y=temp;
+}
+int main(){
+    int a,b;
+    printf("enter two numbers:");
+    scanf("%d %d",&a,&b);
+
+    printf("before swapping : a=%d,b=%d\n",a,b);
+
+    //call swap function
+    swap(&a,&b);
+    printf("after swapping : a=%d,b=%d\n",a,b);
+    return 0;
+}
