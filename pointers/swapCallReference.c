0b5ce38d9db8d00ed427b97c3a3c22980280c873
Prachi Tiwari <tiwariprachi3007@gmail.com>
2026-01-28 21:34:55 +0530

    pointers

--- pointers/swapCallReference.c
+++ pointers/swapCallReference.c
@@ -12,8 +12,6 @@ int main(){
     scanf("%d %d",&a,&b);
 
     printf("before swapping : a=%d,b=%d\n",a,b);
-
-    //call swap function
     swap(&a,&b);
     printf("after swapping : a=%d,b=%d\n",a,b);
     return 0;
