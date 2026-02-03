76bcaf16666b88fa712c39141d4b6a7ed45769dc
Prachi Tiwari <tiwariprachi3007@gmail.com>
2026-02-03 23:18:08 +0530

    Update findMaxElement.c and add stats5.c

--- pointers/findMaxElement.c
+++ pointers/findMaxElement.c
@@ -4,7 +4,6 @@ int main(){
     int arr[100];
     int *ptr;
     int max;
-
     printf("enter the number of elements:");
     scanf("%d",&n);
     printf("enter elements:\n");
---
+++ pointers/stats5.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+
+void stats_of_five(int a,int b,int c,int d,int e, int *sum,double *avg,double *stst_dev){
+    int number[5]={a,b,c,d,e};
+    int i;
+    //sum
+    *sum=0;
+    for(i=0;i<5;i++){
+        *sum=*sum+numbers[i];
+    
+    }
+}
+int main(){
+
+}
